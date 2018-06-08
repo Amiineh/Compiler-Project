fe62db7341ce205e6dae706129b6bb8a93e7db3c
amineh.ahm <amineh.ahm@gmail.com>
2018-06-08 16:31:58 +0430

    removed scanner prints

--- scanner.py
+++ scanner.py
@@ -12,10 +12,10 @@ ID = "ID"
 NUM = "NUM"
 
 
-class state(object):
-    def __init__(self):
-        self.next = {}  # dictionry with key of Token and value of an state
-        self.sub_DFA_name = None
+# class state(object):
+#     def __init__(self):
+#         self.next = {}  # dictionry with key of Token and value of an state
+#         self.sub_DFA_name = None
 
 
 file_name = "testFile.txt"
@@ -29,21 +29,15 @@ class scanner(object):
         input_file = open(file_name)
         self.inputCode = input_file.read()
         self.lastToken = None
-        print(otherTokens)
-
-        print("scanner creation")
 
     def get_char(self):
         if self.currentIndex >= len(self.inputCode):
             return -1
         char = self.inputCode[self.currentIndex]
         self.currentIndex += 1
-        print("get new char , ", char, "index: ", self.currentIndex)
         return char
 
     def get_back(self):
-        print("get back", "index: ", self.currentIndex)
-
         self.currentIndex -= 1
 
     def get_next_token(self):
@@ -56,39 +50,31 @@ class scanner(object):
 
         if char == "+" or char == "-":
             if self.lastToken in operators or self.lastToken in otherTokens:
-                print("charactor is number , ", char)
                 self.handle_num()
             else:
-                print("charactor is token , ", char)
                 self.return_token(char, OTHER)
 
         elif char.isdigit():
-            print("charactor is digit ", char)
             self.handle_num()
 
         elif char in otherTokens or char in operators:
-            print("charactor is token , " , char)
             self.return_token(char , OTHER)
 
         elif char.isalpha():
-            print("charactor is letter ", char)
             self.handle_id()
 
         elif char == "/":
-            print("charactor is BACKSLASH")
             self.handle_comment()
 
         elif char in whitespace:
-            print("whiteSpace")
             return self.get_next_token()
 
         else:
-            print("problem in input charactor")
+            print("problem in input character")
 
     def handle_equal(self):
         nextChar = self.get_char()
         if nextChar == -1:
-            print("file ended , error in scanner. need NUM or ID after token =")
             return
         if nextChar == "=":
             currentToken = "=="
@@ -107,7 +93,6 @@ class scanner(object):
             if newChar.isdigit():
                 currentNum += newChar
             else:
-                print("non digit character after a num")
                 self.get_back()
                 return self.return_token(currentNum, NUM)
 
@@ -121,7 +106,6 @@ class scanner(object):
             if newChar.isdigit() or newChar.isalpha():
                 currentID += newChar
             else:
-                print("non digit or letter character in an identifier")
                 self.get_back()
                 return self.return_token(currentID, ID)
 
@@ -148,9 +132,4 @@ class scanner(object):
         else:
             print("problem: token is not in category: ", token)
 
-
-SC = scanner()
-token = ""
-while token != "EOF":
-    token = SC.get_next_token()
 
